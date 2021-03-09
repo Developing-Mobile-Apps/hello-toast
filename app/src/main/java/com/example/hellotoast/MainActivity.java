74f8a6ee68e2518abd36b39ade66a0057a88d575
bc-dipesh <bcdipeshwork@gmail.com>
2021-03-09 16:20:59 +0545

    FEAT: Add click Handler
    Add click handler for button_toast and button_count

--- app/src/main/java/com/example/hellotoast/MainActivity.java
+++ app/src/main/java/com/example/hellotoast/MainActivity.java
@@ -3,12 +3,34 @@ package com.example.hellotoast;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    private int mCount = 0;
+    private TextView mShowCount;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // get reference of show_count TextView
+        mShowCount = findViewById(R.id.show_count);
+    }
+
+    public void showToast(View view) {
+        Toast.makeText(this, R.string.toast_message, Toast.LENGTH_SHORT).show();
+    }
+
+    public void countUp(View view) {
+        ++mCount;
+
+        // set the text in show_count TextView to the value of mCount
+        if (mShowCount != null) {
+            mShowCount.setText(Integer.toString(mCount));
+        }
     }
 }
